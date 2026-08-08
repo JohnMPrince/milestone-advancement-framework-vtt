46d80e3fdbd668d2f666ba96e7ca522f6f61521b
John Prince <JohnMPrince200@gmail.com>
2026-08-08 13:29:17 +1000

    MAF-1.3 Configured Vite build pipeline.

---
+++ src/hooks/ready.ts
@@ -0,0 +1,1 @@
+// src/hooks/ready.ts
---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+// vite.config.ts
+
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+
+    lib: {
+      entry: resolve(__dirname, "src/main.ts"),
+      formats: ["es"],
+      fileName: () => "milestone-advancement-framework.js",
+    },
+
+    rollupOptions: {
+      output: {
+        entryFileNames: "milestone-advancement-framework.js",
+      },
+    },
+
+    sourcemap: true,
+    minify: false,
+  },
+});
